5a2ab594796ad6f78aed0113b6f3dad6b1b1f9d2
Gary Bolin <garybolin@ivytech.edu>
2026-05-04 17:51:30 -0400

    another one

--- models/teacher.js
+++ models/teacher.js
@@ -3,14 +3,14 @@ const db = require("../db");
 const Teacher = db.model("Teacher", {
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  teacherID: { type: Number, required: true, unique: true },
+  teacherID: { type: Number, required: true, unique: true }, // Mongoose checks this FIRST
   email: { type: String, unique: true, lowercase: true },
   loginName: { type: String, unique: true },
   password: String,
   role: { type: String, default: "teacher" }
 });
 
-
+// CHANGE THIS: Use "validate" instead of "save"
 Teacher.schema.pre("validate", async function (next) {
   if (!this.isNew) return next();
 
@@ -19,7 +19,7 @@ Teacher.schema.pre("validate", async function (next) {
     this.teacherID = lastTeacher && lastTeacher.teacherID ? lastTeacher.teacherID + 1 : 1;
     next();
   } catch (err) {
-    next(err);
+    next(err); // Passes the error to the save() promise
   }
 });
 
