852f19d8f26be9b8261866e858fe4490617642c0
Gary Bolin <garybolin@ivytech.edu>
2026-05-04 17:44:37 -0400

    adjusted teacher model

--- models/teacher.js
+++ models/teacher.js
@@ -1,12 +1,25 @@
-const db = require("../db")
+const db = require("../db");
 
-const Teacher = db.model("Teacher",{
-    firstName: String,
-    lastName: String,
-    email: {type: String, unique:true, lowercase:true},
-    loginName: {type: String, unique: true},
-    password: String,
-    role: {type: String, default: "teacher"}
-})
+const Teacher = db.model("Teacher", {
+  firstName: String,
+  lastName: String,
+  teacherID: Number,
+  email: { type: String, unique: true, lowercase: true },
+  loginName: { type: String, unique: true },
+  password: String,
+  role: { type: String, default: "teacher" }
+});
+
+Teacher.schema.pre("save", async function (next) {
+  if (!this.isNew) return next();
+
+  try {
+    const lastTeacher = await Teacher.findOne({}, { teacherID: 1 }, { sort: { teacherID: -1 } });
+    this.teacherID = lastTeacher && lastTeacher.teacherID ? lastTeacher.teacherID + 1 : 1;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = Teacher;
