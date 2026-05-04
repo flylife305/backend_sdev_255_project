ea27c17f3cdf00de03f9179daed117178825cd40
Gary Bolin <garybolin@ivytech.edu>
2026-05-04 17:54:29 -0400

    another one

--- models/teacher.js
+++ models/teacher.js
@@ -1,26 +1,31 @@
 const db = require("../db");
 
-const Teacher = db.model("Teacher", {
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  teacherID: { type: Number, required: true, unique: true }, // Mongoose checks this FIRST
+const teacherSchema = new db.Schema({
+  firstName: String,
+  lastName: String,
+  teacherID: { type: Number, unique: true, required: true },
   email: { type: String, unique: true, lowercase: true },
   loginName: { type: String, unique: true },
   password: String,
   role: { type: String, default: "teacher" }
 });
 
-// CHANGE THIS: Use "validate" instead of "save"
-Teacher.schema.pre("validate", async function (next) {
-  if (!this.isNew) return next();
 
-  try {
-    const lastTeacher = await Teacher.findOne({}, { teacherID: 1 }, { sort: { teacherID: -1 } });
-    this.teacherID = lastTeacher && lastTeacher.teacherID ? lastTeacher.teacherID + 1 : 1;
-    next();
-  } catch (err) {
-    next(err); // Passes the error to the save() promise
+teacherSchema.pre("validate", async function (next) {
+  if (this.isNew && !this.teacherID) {
+    try {
+      // Access the model through the constructor to avoid circular dependency
+      const Teacher = this.constructor; 
+      const lastTeacher = await Teacher.findOne({}, { teacherID: 1 }, { sort: { teacherID: -1 } });
+      this.teacherID = lastTeacher && lastTeacher.teacherID ? lastTeacher.teacherID + 1 : 1;
+    } catch (err) {
+      return next(err);
+    }
   }
+  next();
 });
+
+
+const Teacher = db.model("Teacher", teacherSchema);
 
 module.exports = Teacher;
